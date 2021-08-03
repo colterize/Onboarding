2b4cd91bb131a32a0bf512027a4fbf5cb72fe7e5
colterize <85697865+colterize@users.noreply.github.com>
2021-08-03 22:14:53 +0700

    Initial Commit

---
+++ OnboardingView SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardingView SwiftUI
+//
+//  Created by Yani . on 03/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
